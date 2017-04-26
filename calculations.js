e4dfd3969a53ad9495144c11b52406f6a920a1f6
Heiner Bihlmaier <heinerbihl@gmail.com>
2017-04-26 19:45:51 +0200

    deleted old stuff

--- calculations.js
+++ calculations.js
@@ -5,20 +5,6 @@ function giveCRStats (crIn) {
         var response = JSON.parse(xhttp.responseText);
         var crThreshold = response.challengeThresholds;
 
-        /* var output = '<h1>' + 'Stats for Challenge Rating with ' + crThreshold[cr].cr + '</h1>'
-                        + '<ul>'
-                        + '<li>' + '<h5>' + 'Challenge Rating: ' + '</h5>' + crThreshold[cr].cr + '</li>'
-                        + '<li>' + '<h5>' + 'Proficiency: ' + '</h5>' + crThreshold[cr].prof + '</li>'
-                        + '<li>' + '<h5>' + 'Armor Class: ' + '</h5>' + crThreshold[cr].ac + '</li>'
-                        + '<li>' + '<h5>' + 'Hit Points Maximum: ' + '</h5>' + crThreshold[cr].hplow + '</li>'
-                        + '<li>' + '<h5>' + 'Hit Points Minimum: ' + '</h5>' + crThreshold[cr].hphigh + '</li>'
-                        + '<li>' + '<h5>' + 'Attack Bonus: ' + '</h5>' + crThreshold[cr].atk + '</li>'
-                        + '<li>' + '<h5>' + 'Damage per round (max): ' + '</h5>' + crThreshold[cr].dprlow + '</li>'
-                        + '<li>' + '<h5>' + 'Damage per round (min): ' + '</h5>' + crThreshold[cr].dprhigh + '</li>'
-                        + '<li>' + '<h5>' + 'Save Difficulty Check: ' + '</h5>' + crThreshold[cr].sdc + '</li>'
-                        + '</ul>' + '<br>';
-        document.getElementById('crDisplay').innerHTML = output; */
-
         var cr = crThreshold[crIn].cr;
         var prof = crThreshold[crIn].prof;
         var ac = crThreshold[crIn].ac;
