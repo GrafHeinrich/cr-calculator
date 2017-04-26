048403aab935e4291215747887601b655fb4bc44
Heiner Bihlmaier <heinerbihl@gmail.com>
2017-04-26 18:31:52 +0200

    added output for first 5 CRs

--- calculations.js
+++ calculations.js
@@ -1,23 +1,22 @@
-function clickFunction () {
+function giveCRStats (cr) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
         var response = JSON.parse(xhttp.responseText);
         var crThreshold = response.challengeThresholds;
 
-        var output = '';
-        for(var i = 0; i < crThreshold.length; i++) {
-            output += '<ul>'+ '<li>' + crThreshold[i].cr+'</li>'+
-                        '<li>' + crThreshold[i].prof+'</li>'+
-                        '<li>' + crThreshold[i].ac+'</li>'+
-                        '<li>' + crThreshold[i].hplow+'</li>'+
-                        '<li>' + crThreshold[i].hphigh+'</li>'+
-                        '<li>' + crThreshold[i].atk+'</li>'+
-                        '<li>' + crThreshold[i].dprlow+'</li>'+
-                        '<li>' + crThreshold[i].dprhigh+'</li>'+
-                        '<li>' + crThreshold[i].sdc+'</li>'+ '</ul>' +
-                        '</hr>';
-        }
+        var output = '<h1>' + 'Stats for Challenge Rating with ' + crThreshold[cr].cr + '</h1>'
+                        + '<ul>'
+                        + '<li>' + '<h5>' + 'Challenge Rating: ' + '</h5>' + crThreshold[cr].cr + '</li>'
+                        + '<li>' + '<h5>' + 'Proficiency: ' + '</h5>' + crThreshold[cr].prof + '</li>'
+                        + '<li>' + '<h5>' + 'Armor Class: ' + '</h5>' + crThreshold[cr].ac + '</li>'
+                        + '<li>' + '<h5>' + 'Hit Points Maximum: ' + '</h5>' + crThreshold[cr].hplow + '</li>'
+                        + '<li>' + '<h5>' + 'Hit Points Minimum: ' + '</h5>' + crThreshold[cr].hphigh + '</li>'
+                        + '<li>' + '<h5>' + 'Attack Bonus: ' + '</h5>' + crThreshold[cr].atk + '</li>'
+                        + '<li>' + '<h5>' + 'Damage per round (max): ' + '</h5>' + crThreshold[cr].dprlow + '</li>'
+                        + '<li>' + '<h5>' + 'Damage per round (min): ' + '</h5>' + crThreshold[cr].dprhigh + '</li>'
+                        + '<li>' + '<h5>' + 'Save Difficulty Check: ' + '</h5>' + crThreshold[cr].sdc + '</li>'
+                        + '</ul>' + '<br>';
         document.getElementById('crDisplay').innerHTML = output;
         }
     };
